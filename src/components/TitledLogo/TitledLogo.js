863b609418c8483ccec2bdccc541495d4fb48a5c
chandlergreff <I433816@fhict.nl>
2022-01-30 05:57:48 +0100

    added titledlogo comp

---
+++ src/components/TitledLogo/TitledLogo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './TitledLogo.css'
+
+export default function TitledLogo() {
+  return <div className='logo'>
+    <img src='./logo_alt_1.svg' />
+    <p>Ingenios</p>
+  </div>;
+}
